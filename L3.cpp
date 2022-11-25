bd58391c35e75e0b8853ab130a03215dfab0baff
MinaTower <116684345+MinaTower@users.noreply.github.com>
2022-11-25 20:42:12 +0300

    Update L3.cpp
    Поменяны местами filler и colunm. Проверен подаваемый массив, подправлено создание 2d д массива.
    step преобразовано в res, итоговый изменён с нуля. 
    
    Будущее: проверка ввода значений n=2,3. Проверка собирания res.

--- L3.cpp
+++ L3.cpp
@@ -14,33 +14,33 @@ int exponentiation(int num, int degree)
 
 int determinant(const int SIZE, int num_element, int* matrix_previous[])
 {
-    int res = 0;
+    int res;
     int** matrix_next_step = new int*[SIZE];
     for (int row = 0; row < SIZE; row++ )
     {
-        int filler = 0;
+        int column = 0;
         matrix_next_step[row] = new int[SIZE];
-        for (int column = 0; column < SIZE; column++)
+        for (int filler = 0; filler < SIZE +1; filler++)
         {
-            if (column != num_element){
-                matrix_next_step[row][column] = matrix_previous[((row + 1) * (SIZE + 1))][filler];
-                filler += 1;
+            if (filler != num_element){
+                matrix_next_step[row][column] = matrix_previous[row + 1][filler];
+                column += 1;
             }
         }
     }
+    //cout << "dva"<<endl;//
     if (SIZE == 2){
-        int step = (matrix_next_step[0][0])*(matrix_next_step[1][1]) - ((matrix_next_step[0][1])*(matrix_next_step[1][0]));
+        res = (matrix_next_step[0][0])*(matrix_next_step[1][1]) - ((matrix_next_step[0][1])*(matrix_next_step[1][0]));
         for (int j=0; j < 2; j++)
         {
             delete[] matrix_next_step[j];
         }
         delete[] matrix_next_step;
-        cout << 'r';//
-        return step;
     } else {
+        //cout <<"Det"<<endl;//
         for (int i = 0; i < SIZE; i++)
         {
-        res += *matrix_previous[i]*exponentiation(-1, (i+1)+1)*determinant(SIZE -1, i, &matrix_next_step[0]);
+        res += (matrix_next_step[0][i])*exponentiation(-1, (i+1)+1)*determinant(SIZE -1, i, &matrix_next_step[0]);
         }
         for (int j=0; j < SIZE; j++)
         {
@@ -48,7 +48,8 @@ int determinant(const int SIZE, int num_element, int* matrix_previous[])
         }
         delete[] matrix_next_step;
     }
-    return 0;
+    cout <<res<<endl;//
+    return res;
 }
 
 int main()
@@ -56,19 +57,18 @@ int main()
     setlocale(LC_ALL, "Russian");
     cout << " Это программа для вычисления определителя матрицы n-го порядка" << endl;
     int answer = 0;
-    const int n = 3;
-    int **matrix_first = new int*;
+    const int n = 4;
+    int** matrix_first = new int*[n];
     for (int row=0;row <n; row ++)
     {
-        matrix_first[row] = new int [n];
+        matrix_first[row] = new int[n];
         for (int column = 0; column < n; column ++)
         {
             //int f;
             //cin >> f;
-            matrix_first[row][column] = (row*n + column);//f;
+            matrix_first[row][column] = (row*n + column+1);//f;
         }
     }
-    cout << 'v';//
     if (n == 2){
         answer = (matrix_first[0][0]*(matrix_first[1][1]) - matrix_first[0][1]*(matrix_first[1][0]));
         for (int j=0; j < n; j++)
