414708c0a524fc2c950229d4e469278d30cacb44
MinaTower <116684345+MinaTower@users.noreply.github.com>
2022-12-09 23:33:36 +0300

    Update L3.cpp
    пробел
    Вопросы русскоязычной поддержки

--- L3.cpp
+++ L3.cpp
@@ -1,91 +1,90 @@
 #include<iostream>
 #include<locale>
 using namespace std;
-
-int exponentiation(int num, int degree)
+struct splitted_string
 {
-    int res = num;
-    for (int i = 2; i <= degree; i++)
-    {
-        res *= num;
-    }
-    return(res);
-}
+    char** words;
+    int num_of_words;
+};
 
-void del(int* matrix[], const int rows)
+splitted_string split(char string[], char separator, int string_len)
 {
-        for (int j=0; j < rows; j++)
-        {
-            delete[] matrix[j];
-        }
-        delete[] matrix;
-}
-
-int minor(const int SIZE, int num_element, int* matrix_previous[])
-{
-    int res = 0;
-    int** matrix_next_step = new int*[SIZE];
-    for (int row = 0; row < SIZE; row++ )
-    {
-        int column = 0;
-        matrix_next_step[row] = new int[SIZE];
-        for (int filler = 0; filler < SIZE +1; filler++)
-        {
-            if (filler != num_element){
-                matrix_next_step[row][column] = matrix_previous[row + 1][filler];
-                column += 1;
-            }
-        }
-    }
-    if (SIZE == 2){
-        res = (matrix_next_step[0][0])*(matrix_next_step[1][1]) - ((matrix_next_step[0][1])*(matrix_next_step[1][0]));
-        del(&matrix_next_step[0], SIZE);
-    } else {
-        for (int i = 0; i < SIZE; i++)
-        {
-        res += (matrix_next_step[0][i])*exponentiation(-1, (i+1)+1)*minor(SIZE -1, i, &matrix_next_step[0]);
-        }
-        del(&matrix_next_step[0], SIZE);
-    }
-    return res;
-}
-
-int determinant(int* matrix_1[], const int size)
-{
-    int ans =0;
-    if (size == 2){
-        ans = (matrix_1[0][0]*(matrix_1[1][1]) - matrix_1[0][1]*(matrix_1[1][0]));
-    } else {
-        for (int i = 0; i < size; i++)
-        {
-            ans += (matrix_1[0][i])*exponentiation(-1, (i+1)+1)*minor(size - 1, i, &matrix_1[0]);
-        }
-    }
-    return ans;
+    setlocale(LC_ALL, "Russian");
+    cout << separator << ' j' << endl;
+    splitted_string ans;
+	int separator_num_of_ind = 0;
+	ans.num_of_words = 1;
+	for (int i=0; i<string_len + 1; i++)
+	{
+		if (string[i] == separator && (i != string_len)){
+			separator_num_of_ind +=1;
+			ans.num_of_words +=1;
+		} else if (i == 0 || i == string_len){
+			separator_num_of_ind +=1;
+	    }
+	}
+	if (ans.num_of_words == 1 && string[string_len]!=separator){
+		ans.words = &string;
+		return ans;
+	}
+	int* separator_ind = new int[separator_num_of_ind];
+	int j =0 ;
+	for (int i=0; i<string_len; i++)
+	{
+		if (string[i] == separator){
+			separator_ind[j] = i;
+			j++;
+		} else if (i == 0 || i == string_len-1 ){
+			separator_ind[j] = i;
+			j++;
+		}
+	}
+	ans.words = new char*[ans.num_of_words];
+	for (int word_n =0; word_n < ans.num_of_words; word_n++)
+	{
+		int differ = separator_ind[word_n + 1] - separator_ind[word_n] ;
+		if ((word_n == 0 && string[0] != separator) || (word_n == ans.num_of_words - 1 && (string[string_len-1] != separator))){
+		    differ += 1;
+		}
+		ans.words[word_n] = new char[differ];
+		for (int letter_n =0;letter_n < differ - 1;letter_n ++)
+		{
+		    int ind = separator_ind[word_n] + 1 + letter_n;
+		    if (word_n == 0 && (string[0] != separator))
+		    {
+		        ind -= 1;
+		    }
+			ans.words[word_n][letter_n] = string[ind];
+		}
+		ans.words[word_n][differ -1] = '\0';
+	}
+	//for (int h=0;h < ans.num_of_words; h++)
+	//{
+	//    cout << ans.words[h];
+	//};
+	//cout << endl;
+	return ans;
 }
 
 int main()
 {
+   // system("cp1251");
     setlocale(LC_ALL, "Russian");
-    cout << " Это программа для вычисления определителя матрицы n-го порядка" << endl;
-    cout << " Введите порядок исследуемой матрицы: ";
-    int n;
-    cin >> n;
-    cout << " Вводите значения элементов матрицы построчно (a[1][1], следом a[1][2] и т.д.)" << endl;
-    int** matrix_first = new int*[n];
-    for (int row=0;row < n; row ++)
-    {
-        matrix_first[row] = new int[n];
-        for (int column = 0; column < n; column ++)
-        {
-            int f;
-            cin >> f;
-            matrix_first[row][column] = f;
-        }
-    }
-    int answer = 0;
-    answer = determinant(&matrix_first[0], n);
-    cout << "Определитель равен " << answer << endl;
-    del(&matrix_first[0], n);
-    return 0;
+    cout << " Это программа для разделения строки на подстроки по вводимому символу" << endl<<" Ваш символ - пробел(y/n)? " ;
+    char if_space;
+    char sep = ' ';
+    cin >> if_space;
+    if (if_space == 'n'){
+    	cout <<" Укажите его: ";//цифры
+    	cin >> sep;
+    }// для не-дурака
+	char mystr[] = "Какой-то текст";/*"Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversations?'"*/ //"my secret servise";
+	int mystr_len = sizeof(mystr) - 1;
+	splitted_string subs = split(&mystr[0], sep, mystr_len);//sep
+	cout << sep <<endl<< "Полученный результат:"<< endl;
+	for (int h=0;h < subs.num_of_words; h++)
+	{
+	    cout << subs.words[h]<<endl;//
+	}
+	return 0;
 }
